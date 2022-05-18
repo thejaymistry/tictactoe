5d4e45d3d7f0c8b1d984d808d97ca0fe4caa8f7a
thejaymistry <48210904+thejaymistry@users.noreply.github.com>
2022-05-18 17:40:21 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print("Hello!!")
+import math
+import random
+
+class Player:
+  def __init__(self, letter):
+    self.letter=leter
+    
+  def get_move(self,game):
+    pass
+  
+ class randomcomputerplayer(Player):
+  
